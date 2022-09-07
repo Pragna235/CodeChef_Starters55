f21ec83337578ff0ec7c7f7b8b099ece099717f9
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2022-09-07 22:48:49 +0530

    Add files via upload
    FEVER...

---
+++ fever.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int i, T, X;
+	scanf("%d",&T);
+	for(i=0;i<T;i++)
+	{scanf("%d",&X);
+
+	if(X>98)
+	{printf("YES");}
+	else
+	{printf("NO");}
+	    printf("\n");
+	}
+	return 0;
+}
+
+
